d227ae455770ff2f8c79979c3ff5cb7a381a5abc
bxm <1759567121@qq.com>
2019-08-20 16:10:41 +0800

    alter

--- test.py
+++ test.py
@@ -1,6 +1,35 @@
-import tensorflow as tf
+import json
+import tqdm
 
-a = tf.range(10)*5
+data_train = []
+data_test = []
+data_dev = []
+count = 0
 
-with tf.Session() as sess:
-    print(sess.run(a))
+# with open('testdata/trainset.txt') as f:
+#     for idx, line in enumerate(f):
+#         if idx % 100000 == 0:
+#             print('read train file line %d' % idx)
+#         data_train.append(json.loads(line))
+#         if idx == 100000:
+#             break
+
+# with open('testdata/validset.txt') as f:
+#     for idx, line in enumerate(f):
+#         data_dev.append(json.loads(line))
+# print("dev_len", len(data_dev))
+#
+# with open('testdata/testset.txt') as f:
+#     for idx, line in enumerate(f):
+#         data_test.append(json.loads(line))
+# print("test_len", len(data_test))
+
+# for test in tqdm.tqdm(data_test):
+#     for train in data_train:
+#         if test == train:
+#             count += 1
+#             print("test:", test)
+#             print("train:", train)
+# print(count)
+
+print((int)(8000 / 5000))
